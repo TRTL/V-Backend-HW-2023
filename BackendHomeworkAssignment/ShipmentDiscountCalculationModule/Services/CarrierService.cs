9aa4408480ce278db470a62130d7c0d527377108
TRTL <4seven@gmail.com>
2023-03-22 01:25:19 +0200

    added CarrierService, added ConsoleService

---
+++ BackendHomeworkAssignment/ShipmentDiscountCalculationModule/Services/CarrierService.cs
@@ -0,0 +1,19 @@
+﻿using ShipmentDiscountCalculationModule.Data;
+using ShipmentDiscountCalculationModule.Models;
+using ShipmentDiscountCalculationModule.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipmentDiscountCalculationModule.Services
+{
+    internal class CarrierService : ICarrierService
+    {
+        public void GetCarrierPrice(Shipment shipment)
+        {
+            shipment.ShipmentPrice = ProviderPriceData.priceList.Find(p => p.PackageSize == shipment.PackageSizeCode && p.Provider == shipment.CarrierCode).Price;
+        }
+    }
+}
---
+++ BackendHomeworkAssignment/ShipmentDiscountCalculationModule/Services/ConsoleService.cs
@@ -0,0 +1,19 @@
+﻿using ShipmentDiscountCalculationModule.Models;
+using ShipmentDiscountCalculationModule.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipmentDiscountCalculationModule.Services
+{
+    internal class ConsoleService : IConsoleService
+    {
+        public void PrintWithHyphenWhereNoDiscount(Shipment shipment)
+        {
+            string discount = shipment.ShipmentDiscount == null ? "-" : shipment.ShipmentDiscount.Value.ToString();
+            Console.WriteLine($"{shipment.Date} {shipment.PackageSizeCode} {shipment.CarrierCode} {shipment.ShipmentPrice} {discount} ");
+        }
+    }
+}
