fd871cba7f62c42ca3fa493fa47172d827f25e07
TRTL <4seven@gmail.com>
2023-03-22 01:23:12 +0200

    added CarrierService

---
+++ BackendHomeworkAssignment/ShipmentDiscountCalculationModule/Services/Interfaces/ICarrierService.cs
@@ -0,0 +1,15 @@
+﻿using ShipmentDiscountCalculationModule.Data;
+using ShipmentDiscountCalculationModule.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipmentDiscountCalculationModule.Services.Interfaces
+{
+    internal interface ICarrierService
+    {
+        void GetCarrierPrice(Shipment transaction);
+    }
+}
