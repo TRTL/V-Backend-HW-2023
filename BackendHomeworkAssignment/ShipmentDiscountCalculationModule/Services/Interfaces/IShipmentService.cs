fc250402c6254afe271e0644b510224d13118b8b
TRTL <4seven@gmail.com>
2023-03-22 01:22:47 +0200

    added ShipmentService

---
+++ BackendHomeworkAssignment/ShipmentDiscountCalculationModule/Services/Interfaces/IShipmentService.cs
@@ -0,0 +1,14 @@
+﻿using ShipmentDiscountCalculationModule.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipmentDiscountCalculationModule.Services.Interfaces
+{
+    internal interface IShipmentService
+    {
+        void ApplyDiscountToPrice(Shipment shipment);
+    }
+}
---
+++ BackendHomeworkAssignment/ShipmentDiscountCalculationModule/Services/ShipmentService.cs
@@ -0,0 +1,18 @@
+﻿using ShipmentDiscountCalculationModule.Models;
+using ShipmentDiscountCalculationModule.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipmentDiscountCalculationModule.Services
+{
+    internal class ShipmentService : IShipmentService
+    {
+        public void ApplyDiscountToPrice(Shipment shipment)
+        {
+            if (shipment.ShipmentDiscount != null) shipment.ShipmentPrice -= shipment.ShipmentDiscount;
+        }
+    }
+}
